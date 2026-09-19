9949f1b089969f657d84328bc2a6416622fd907f
Oksana Goncharova <oksanagoncharova2011@gmail.com>
2026-09-19 19:17:54 +0800

    0123

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-//
+//task1
+    System.out.println("task1");
+
+    int age = 2;
+    if (age >= 18) {
+        System.out.println("Если возраст человека =" + age + ", то он совершеннолетний");
+    } else {
+        System.out.println("Если возраст человека =" + age + ", то он несовершеннолетний");
+    }
+    //task 2
+    System.out.println( "task 2");
+
+
 }
